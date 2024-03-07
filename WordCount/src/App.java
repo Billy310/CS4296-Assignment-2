d439fb4e45728ba04ea1035264f8b34c08ba05d8
Billy Gor <120034159+Billy310@users.noreply.github.com>
2024-03-07 13:25:17 +0800

    Update

--- WordCount/src/App.java
+++ WordCount/src/App.java
@@ -1,61 +1,67 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configuration;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.*;
 
 public class App {
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
-
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    // private Text word = new Text();
+    private Set<String> dictionary = new HashSet<>(Arrays.asList(
+        "John", "likes", "to", "watch", "movies",
+        "also", "football", "games", "Mary", "too"));
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      // String[] line = value.toString().split("\\s+");
+      String line = value.toString().toLowerCase(); // Convert to lower case
+      line = line.replaceAll("[^a-z0-9 ]", " "); // Replace non-alphanumeric characters with space
+      String[] words = line.split("\\s+"); // Split on whitespace
+      Arrays.stream(words)
+          .filter(dictionary::contains)
+          .forEach(w -> {
+            try {
+              context.write(new Text(w), one);
+            } catch (IOException | InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+          });
     }
   }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
-
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
+      int sum = StreamSupport.stream(values.spliterator(), false)
+          .mapToInt(IntWritable::get)
+          .sum();
+      context.write(key, new IntWritable(Math.min(sum, Integer.MAX_VALUE)));
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
+    Job job = Job.getInstance(conf, "bagofwords");
     job.setJarByClass(App.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path("s3://assignment2yuentatshingbilly/input.txt"));
-    FileOutputFormat.setOutputPath(job, new Path("s3://assignment2yuentatshingbilly/output.txt"));
+    // FileInputFormat.addInputPath(job, new Path(args[0]));
+    // FileInputFormat.addInputPath(job, new Path("s3://assignment2yuentatshingbilly/input.txt"));
+
+    FileInputFormat.addInputPath(job, new Path("s3://assignment2yuentatshingbilly/testFiles"));
+    FileOutputFormat.setOutputPath(job, new Path("s3://assignment2yuentatshingbilly/output"));
+
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
+
 }
