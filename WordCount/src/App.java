c5a70aa140c9d60ee89f50fcf8511c6d65bd86bf
Billy Gor <120034159+Billy310@users.noreply.github.com>
2024-03-15 10:40:51 +0800

    update

--- WordCount/src/App.java
+++ WordCount/src/App.java
@@ -1,7 +1,5 @@
-
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
@@ -9,19 +7,30 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 public class App {
 
   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
-    // private Text word = new Text();
-    private Set<String> dictionary = new HashSet<>(Arrays.asList(
-        "John", "likes", "to", "watch", "movies",
-        "also", "football", "games", "Mary", "too"));
+    private final static String[] top100Word = { "the", "be", "to", "of", "and", "a",
+        "in", "that", "have", "i", "it", "for", "not", "on", "with", "he", "as", "you", "do",
+        "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an",
+        "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if",
+        "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time",
+        "no", "just", "him", "know", "take", "people", "into", "year", "your", "good",
+        "some", "could", "them", "see", "other", "than", "then", "now", "look", "only",
+        "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our",
+        "work", "first", "well", "way", "even", "new", "want", "because", "any", "these",
+        "give", "day", "most", "us" };
+    private Set<String> dictionary = new HashSet<>(Arrays.asList(top100Word));
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-      // String[] line = value.toString().split("\\s+");
+      Path filePath = ((FileSplit) context.getInputSplit()).getPath();
+      String fileName = filePath.getName();
+      // System.out.println(fileName);
+
       String line = value.toString().toLowerCase(); // Convert to lower case
       line = line.replaceAll("[^a-z0-9 ]", " "); // Replace non-alphanumeric characters with space
       String[] words = line.split("\\s+"); // Split on whitespace
@@ -29,7 +38,7 @@ public class App {
           .filter(dictionary::contains)
           .forEach(w -> {
             try {
-              context.write(new Text(w), one);
+              context.write(new Text(w + "@" + fileName), one);
             } catch (IOException | InterruptedException e) {
               throw new RuntimeException(e);
             }
@@ -38,12 +47,27 @@ public class App {
   }
 
   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private MultipleOutputs<Text, IntWritable> multipleOutputs;
+
+    @Override
+    public void setup(Context context) {
+      multipleOutputs = new MultipleOutputs<>(context);
+    }
+
     public void reduce(Text key, Iterable<IntWritable> values, Context context)
         throws IOException, InterruptedException {
       int sum = StreamSupport.stream(values.spliterator(), false)
           .mapToInt(IntWritable::get)
           .sum();
-      context.write(key, new IntWritable(Math.min(sum, Integer.MAX_VALUE)));
+      String[] wordAndFileName = key.toString().split("@");
+      String word = wordAndFileName[0];
+      String fileName = wordAndFileName[1];
+      multipleOutputs.write(key, new IntWritable(Math.min(sum, Integer.MAX_VALUE)), fileName);
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+      multipleOutputs.close();
     }
   }
 
@@ -55,13 +79,11 @@ public class App {
     job.setReducerClass(Reduce.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
-    // FileInputFormat.addInputPath(job, new Path(args[0]));
-    // FileInputFormat.addInputPath(job, new Path("s3://assignment2yuentatshingbilly/input.txt"));
 
+    job.setNumReduceTasks(1);
     FileInputFormat.addInputPath(job, new Path("s3://assignment2yuentatshingbilly/testFiles"));
     FileOutputFormat.setOutputPath(job, new Path("s3://assignment2yuentatshingbilly/output"));
 
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
-
 }
